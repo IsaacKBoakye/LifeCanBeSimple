4b0ba8aa1d24e572f2e0a2bf859be1c10885c87f
Isaac Boakye <ikboakye2001@gmail.com>
2024-02-16 12:11:10 -0400

    updated2

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ function addNumbers(a, b) {
     return a + b;
 }
 
-const resultElement = document.getElementById('result');
-resultElement.textContent = `Result: ${addNumbers(2, 3)}`;
+// const resultElement = document.getElementById('result');
+// resultElement.textContent = `Result: ${addNumbers(2, 3)}`;
+
+// Export the addNumbers function for testing
+module.exports = { addNumbers };
